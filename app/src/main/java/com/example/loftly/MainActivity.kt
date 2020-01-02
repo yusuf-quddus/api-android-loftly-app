af1ba958e62d3c8d0b1bf3409d755f53b23d9740
Yusuf-Quddus <yusuf_quddus@yahoo.com>
2020-01-02 14:49:47 -0800

    add listing with posting info

--- app/src/main/java/com/example/loftly/MainActivity.kt
+++ app/src/main/java/com/example/loftly/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.unaryPlus
+import androidx.ui.core.*
 import androidx.ui.core.Text
-import androidx.ui.core.dp
-import androidx.ui.core.setContent
-import androidx.ui.layout.Padding
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.surface.Card
 import androidx.ui.foundation.DrawImage
+import androidx.ui.foundation.shape.corner.CircleShape
+import androidx.ui.graphics.Color
+import androidx.ui.layout.*
 import androidx.ui.res.imageResource
+import androidx.ui.text.font.FontWeight
 import androidx.ui.tooling.preview.Preview
+import java.time.format.TextStyle
+import androidx.ui.core.Text as Text1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,11 +33,61 @@ class MainActivity : AppCompatActivity() {
 fun PostItem() {
     Card() {
         Padding(padding = 8.dp) {
-           var image = +imageResource(R.drawable.house1)
-            DrawImage(image)
+           Column() {
+               Container(expanded = true, height = 180.dp) {
+                   var image = +imageResource(R.drawable.house2)
+                   DrawImage(image)
+               }
+               HeightSpacer(height = 16.dp)
+               Text(
+                   "555 Terrace Point",
+                   style = androidx.ui.text.TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
+               )
+
+               HeightSpacer(height = 16.dp)
+               Row() {
+                   Row() {
+                       Container(width = 40.dp, height = 40.dp) {
+                           Clip(shape = CircleShape) {
+                               DrawImage(image = +imageResource(R.drawable.profile2))
+                           }
+                       }
+                       Column(modifier = Spacing(left = 8.dp)) {
+                           Text(
+                               "Papa John",
+                               style = androidx.ui.text.TextStyle(
+                                   fontSize = 14.sp,
+                                   fontWeight = FontWeight.W600
+                               )
+                           )
+                           HeightSpacer(height = 2.dp)
+                           Text(
+                               "Property Owner",
+                               style = androidx.ui.text.TextStyle(
+                                   fontSize = 12.sp,
+                                   color = Color.DarkGray
+                               )
+                           )
+                       }
+                   }
+                   Column() {
+                       Text(
+                           text = "Oct 24, 2019",
+                           style = androidx.ui.text.TextStyle(fontSize = 14.sp, color = Color.LightGray)
+                       )
+                       Text(
+                           text = "200 Applicants",
+                           style = androidx.ui.text.TextStyle(fontSize = 14.sp, color = Color.LightGray))
+
+                   }
+
+               }
+           }
         }
     }
 }
+
+
 
 @Preview
 @Composable
