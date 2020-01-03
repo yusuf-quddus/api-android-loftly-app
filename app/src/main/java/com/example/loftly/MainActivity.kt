1be2d11b8d6d5ec0893f52e0a0714050cfb30d91
Yusuf-Quddus <yusuf_quddus@yahoo.com>
2020-01-02 16:13:26 -0800

    some refactoring

--- app/src/main/java/com/example/loftly/MainActivity.kt
+++ app/src/main/java/com/example/loftly/MainActivity.kt
@@ -30,73 +30,94 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
+fun listingsView(children: @Composable() () -> Unit) {
+    children()
+}
+
+@Composable
 fun PostItem() {
     Card(elevation = 2.dp) {
         Padding(padding = 8.dp) {
-           Column() {
-               Container(expanded = true, height = 180.dp) {
-                   var image = +imageResource(R.drawable.house2)
-                   DrawImage(image)
-               }
-               HeightSpacer(height = 16.dp)
-               Text(
-                   "555 Terrace Point",
-                   style = androidx.ui.text.TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
-               )
-
-               HeightSpacer(height = 16.dp)
-               FlowRow(
-                   mainAxisSize = LayoutSize.Expand,
-                   mainAxisAlignment = MainAxisAlignment.SpaceBetween,
-                   crossAxisAlignment = FlowCrossAxisAlignment.Center
-               )  {
-                   Row {
-                       Container(width = 40.dp, height = 40.dp) {
-                           Clip(shape = CircleShape) {
-                               DrawImage(image = +imageResource(R.drawable.profile2))
-                           }
-                       }
-                       Column(modifier = Spacing(left = 8.dp)) {
-                           Text(
-                               "Papa John",
-                               style = androidx.ui.text.TextStyle(
-                                   fontSize = 14.sp,
-                                   fontWeight = FontWeight.W600
-                               )
-                           )
-                           HeightSpacer(height = 2.dp)
-                           Text(
-                               "Property Owner",
-                               style = androidx.ui.text.TextStyle(
-                                   fontSize = 12.sp,
-                                   color = Color.DarkGray
-                               )
-                           )
-                       }
-                   }
-                   Column() {
-                       Text(
-                           text = "Oct 24, 2019",
-                           style = androidx.ui.text.TextStyle(fontSize = 14.sp, color = Color.DarkGray)
-                       )
-                       Text(
-                           text = "200 Applicants",
-                           style = androidx.ui.text.TextStyle(fontSize = 14.sp, color = Color.DarkGray))
-
-                   }
-
-               }
+           Column {
+              listingImage();
+               listingInfo()
            }
         }
     }
 }
 
+@Composable
+fun listingImage() {
+    Container(expanded = true, height = 180.dp) {
+        var image = +imageResource(R.drawable.house2)
+        DrawImage(image)
+    }
+}
 
+@Composable
+fun listingInfo() {
+    HeightSpacer(height = 16.dp)
+    Address()
+    HeightSpacer(height = 16.dp)
+    FlowRow(
+        mainAxisSize = LayoutSize.Expand,
+        mainAxisAlignment = MainAxisAlignment.SpaceBetween,
+        crossAxisAlignment = FlowCrossAxisAlignment.Center
+    )  {
+        AuthorInfo()
+    }
+}
+
+@Composable
+fun Address() {
+    Text(
+        "555 Terrace Point",
+        style = androidx.ui.text.TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
+    )
+}
+
+@Composable
+fun AuthorInfo() {
+    Row {
+        Container(width = 40.dp, height = 40.dp) {
+            Clip(shape = CircleShape) {
+                DrawImage(image = +imageResource(R.drawable.profile2))
+            }
+        }
+        Column(modifier = Spacing(left = 8.dp)) {
+            Text(
+                "Papa John",
+                style = androidx.ui.text.TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W600
+                )
+            )
+            HeightSpacer(height = 2.dp)
+            Text(
+                "Property Owner",
+                style = androidx.ui.text.TextStyle(
+                    fontSize = 12.sp,
+                    color = Color.DarkGray
+                )
+            )
+        }
+    }
+    Column() {
+        Text(
+            text = "Oct 24, 2019",
+            style = androidx.ui.text.TextStyle(fontSize = 14.sp, color = Color.DarkGray)
+        )
+        Text(
+            text = "200 Applicants",
+            style = androidx.ui.text.TextStyle(fontSize = 14.sp, color = Color.DarkGray))
+
+    }
+}
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
+    listingsView {
         PostItem()
     }
 }
